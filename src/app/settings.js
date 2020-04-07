0018fa226b895b08856e24f3003615570f051a81
gusflopes <gusflopes86@gmail.com>
2020-04-06 21:10:32 -0400

    feat: small changes to get user details

--- src/app/settings.js
+++ src/app/settings.js
@@ -1,3 +1,12 @@
 import React from 'react'
-const Settings = () => <p>Settings</p>
+const Settings = ({ user }) => (
+  <div>
+    <h1>Profile</h1>
+    <img src={user.picture} alt="Profile" />
+
+    <h2>{user.name}</h2>
+    <p>{user.email}</p>
+    <code>{JSON.stringify(user, null, 2)}</code>
+  </div>
+)
 export default Settings
