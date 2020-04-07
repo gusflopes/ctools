7758275b28f58b97842130b9e9438f5520d9059a
gusflopes <gusflopes86@gmail.com>
2020-04-06 22:48:31 -0400

    feat: example on usign dinamic content from api

--- src/app/api-example.js
+++ src/app/api-example.js
@@ -2,24 +2,46 @@ import React, { useState, useEffect } from 'react'
 import { api } from '../utils/api';
 
 const ApiExample = ({ user }) => {
-  // const [input, setInput] = useState();
+  const [input, setInput] = useState('');
+  const [loading, setLoading] = useState(true);
   const [githubUser, setGithubUser] = useState();
   // const {email} = user
 
-  // useEffect(() => {
-  //   console.log('recebeu resposta do Github')
-  //   console.log(githubUser)
-  // }, [githubUser])
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    setLoading(true)
+    setGithubUser('')
+    await fetchGithub()
+  }
+
+  const fetchGithub = async () => {
+    try {
+      const user = await api.get(`/users/${input}`)
+      setGithubUser(user)
+      setLoading(false);
+      return setInput('');
+    } catch (err) {
+      console.log(err);
+      setLoading(false)
+    }
+  }
+
 
   useEffect(() => {
     (async () => {
-      const user = await api.get(`/users/gusflopes`)
-      setGithubUser(user);
+      setLoading(false)
     })()
   }, [])
 
   return (
     <div>
+      <div>
+        <form onSubmit={event => handleSubmit(event)}>
+          <label htmlFor="githubUser" >Pesquisar usuário do Github</label>
+          <input name="githubUser" value={input} onChange={e => setInput(e.target.value)} />
+          <button type="submit">Pesquisar</button>
+        </form>
+      </div>
       <div>
         <h1>Authenticated User</h1>
         <h2>{user.name}</h2>
@@ -28,12 +50,12 @@ const ApiExample = ({ user }) => {
       </div>
       <div>
         <h1>Github User</h1>
-        {githubUser && (
+        {loading ? <h2>Carregando...</h2> : githubUser ? githubUser && (
           <>
             <h2>{githubUser.login}</h2>
             <p>{githubUser.email}</p>
             <code>{JSON.stringify(githubUser, null, 2)}</code>
-          </>)}
+          </>) : <h2>Usuário não localizado</h2>}
       </div>
     </div>
   )
