7595dcb882f007d34dfb6a6d9ac09dbde003d90c
gusflopes <gusflopes86@gmail.com>
2020-04-13 09:56:59 -0400

    chore: working on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,46 @@
-import React, { useState } from "react"
+import React from "react"
 import { Link } from 'gatsby'
 import { Layout } from '../components/layout'
 import { Video } from '../components/video'
-import { Button } from 'react-bootstrap'
+import { Jumbotron, Button, Row, Col } from 'react-bootstrap'
 import axios from 'axios';
-import MyDropZone from '../components/dropzone';
 import { toast } from 'react-toastify'
 // <iframe width="560" height="315" src="https://www.youtube.com/embed/w9TKHsKNlPI?start=3" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 
 export default () => {
-  const [file, setFile] = useState();
-
-  async function handleFile(inputFile) {
-    if (file !== undefined) return toast.error('Arquivo já importado. Para substituir recarregue a página.')
-    const newFile = inputFile[0]
-    if (newFile.type !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
-      return toast.error('Formato não suportado. Utilize a planilha exemplo ou o formulário.');
-    }
-    setFile(newFile);
-    // const response = await importExcel(newFile); // voltar  Interface
-    alert('need to fix')
-    // if (process.env.NODE_ENV === 'development') console.log('final', response);
-    alert('need to fix')
-    // return setImported(response)
-  }
+// FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDFDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDFFDFDFFDFDFDFDFGDFFDFFDFFFDDFF
 
   async function lambdaRequest() {
     // fetch("/.netlify/functions/hello")
     //   .then(response => response.json())
     //   .then(console.log)
-
     const response = await axios.get('/.netlify/functions/hello')
     console.log(response);
   }
+
   return (
     <Layout>
-      <p>Hello World</p>
-      <Link to="/app">Go to App</Link>{' '}
-      <Link to="/blog">Go to Blog</Link>{' '}
-      <Link to="/importador">Go to Importador</Link>{' '}
-      <Button variant="primary" onClick={lambdaRequest}>Lambda</Button>
-      <Button variant="primary" onClick={() => toast.error('Deu certo!?')}>Toast</Button>
-
-      <MyDropZone file={file} handleFile={handleFile} />
+      {/* <div style={{backgroundColor: 'blue'}}>OLHA só</div> */}
+      <Jumbotron>
+        <Col lg="8" md="8" sm="12" xs="12">
+        <h1>CTools</h1>
+        <h3>Importar lançamentos contábeis sem complicação</h3>
+        <p>A Ferramenta Importação de Lançamentos Contábeis da CTools gera automaticamente o arquivo de importação de Lançamentos Contábeis do Domínio Sistemas a partir de uma simples planilha de Excel.</p>
+        <Link to="/instrucoes"><Button style={{margin: '15px 8px 15px 0px'}} variant="dark" onClick={() => toast.error('Deu certo!?')}>Como usar</Button></Link>
+        <Link to="/importador"><Button variant="primary">Importar Lançamentos</Button></Link>
+        {/* <Button variant="success" onClick={() => toast.error('Deu certo!?')}>Franscisco</Button> */}
+        </Col>
+      </Jumbotron>
+    <Row>
+      <Col lg="6" sm="12" style={{backgroundColor: '#7159c1'}}>
       <Video width="560" height="315" videoUrl="https://www.youtube.com/embed/w9TKHsKNlPI?start=3" videoTitle="CTools" />
+      </Col>
+      <Col lg="4" sm="12">
+        <h4>Some tagline here</h4>
+      </Col>
+    </Row>
+
     </Layout>
   )
 }
