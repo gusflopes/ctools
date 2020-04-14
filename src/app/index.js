8561434691fb60a85f55dbd7353e79a87712bf29
gusflopes <gusflopes86@gmail.com>
2020-04-13 21:40:41 -0400

    fix: problems with deploy

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const Home = ({ user }) => {
-  return (<h1>Olá, {user.name ? user.name : "amigo"}!</h1>
-    <p>Esta seção ainda está em desenvolvimento. <br />
+  return (
+    <div>
+      <h1>Olá, {user.name ? user.name : "amigo"}!</h1>
+      <p>Esta seção ainda está em desenvolvimento. <br />
   Por enquanto estamos focando nas funcionalidades que estão disponíveis para todos os usuários, não havendo necessidade de utilizar essa área por enquanto. <br />
-  Se você está acessando esta área é provavelmente estamos a utilizando em testes, mas logo será desativada novamente.</p>)
+  Se você está acessando esta área é provavelmente estamos a utilizando em testes, mas logo será desativada novamente.</p>
+    </div>)
 }
 
 export default Home
